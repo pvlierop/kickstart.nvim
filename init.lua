be17baf91944ffaf1c2a4f6412e015413a3166b5
Paul van Lierop <paul.van.lierop@kabisa.nl>
2022-12-15 22:09:21 +0100

    fixc typo

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-:-- Install packer
+-- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
